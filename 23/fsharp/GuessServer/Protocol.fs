9a9903d68036ea5586ae54b4da627bb97bbc8f83
pintset <63047693+pintset@users.noreply.github.com>
2022-08-26 20:41:14 +0200

    HW-18: Guess game (http) (#14)
    * HW-18: Guess game (http)
    
    * HW-18 (23) Moved to folder 23

---
+++ 23/fsharp/GuessServer/Protocol.fs
@@ -0,0 +1,18 @@
+﻿module Protocol
+
+open System
+
+type NewGame =
+    { Min: int
+      Max: int }
+
+type Guess =
+    { Id: Guid
+      Number: int }
+
+type GameResult =
+    | YouWon
+    | GameOver
+    | Greater
+    | Lower
+    | GameNotFound
