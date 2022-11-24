1a015b07054c91fe253c96d27faef7f087a622d2
pintset <63047693+pintset@users.noreply.github.com>
2022-11-24 17:15:21 +0100

    HW-19: WebSockets homework (#15)
    HW-24 WebSocket homework. Guess game

---
+++ 24/scala/src/main/scala/server/routes/Http.scala
@@ -0,0 +1,37 @@
+package server.routes
+
+import cats.effect.Sync
+import common.domain.{GameNotFound, Guess, NewGame}
+import org.http4s.HttpRoutes
+import org.http4s.dsl.Http4sDsl
+import server.GameServer
+import io.circe.generic.auto._
+import cats.syntax.flatMap._
+import org.http4s.circe.CirceEntityCodec._
+import common.domain.GameId.encoder
+import common.domain.AttemptResult.codec
+import GameNotFound.encoder
+
+object Http {
+  def apply[F[_] : Sync](gameService: GameServer[F]): HttpRoutes[F] = {
+    val dsl = new Http4sDsl[F] {}
+    import dsl._
+
+    HttpRoutes.of[F] {
+      case req @ POST -> Root / "guess" =>
+        req
+          .as[Guess]
+          .flatMap { guess =>
+            gameService
+              .guess(guess.gameId, guess.guess)
+              .flatMap(_.fold(NotFound(GameNotFound(guess.gameId)))(Ok(_)))
+          }
+
+      case req @ POST -> Root / "start" =>
+        req
+          .as[NewGame]
+          .flatMap { newGame => gameService.start(newGame.min, newGame.max, newGame.attemptCount) }
+          .flatMap(Created(_))
+    }
+  }
+}
