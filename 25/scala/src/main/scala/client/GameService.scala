584ba88737ce8ff2409d37facc3eecf80460b5bc
Denis Terletskiy <terletskiy@live.com>
2022-11-24 17:12:22 +0100

    HW-19 Folder 25 removed

--- 25/scala/src/main/scala/client/GameService.scala
+++
@@ -1,8 +0,0 @@
-package client
-
-import common.domain.{AttemptResult, GameId, NewGame}
-
-trait GameService[F[_]] {
-  def start(settings: NewGame): F[GameId]
-  def guess(gameId: GameId, guess: Int): F[AttemptResult]
-}
